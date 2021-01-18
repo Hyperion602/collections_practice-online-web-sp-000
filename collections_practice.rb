ae8e9c5164c3bebe11b1376bd57a03495f194894
James Harris <mharris9608@yahoo.com>
2021-01-18 15:02:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,15 @@ end
 def sort_array_desc(arr2)
   arr2.sort.reverse
 end
+
+def sort_array_char_count(arr3)
+  arr3.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+      
